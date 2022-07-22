24b163adb0ddf9514088b6c346028cedca297b17
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-22 09:35:03 +0530

    Create SearchInSinglyLinkedList.java

---
+++ SearchInSinglyLinkedList.java
@@ -0,0 +1,116 @@
+.................
+Main.java
+.................
+class Main{
+    public static void main(String[] args){
+        SinglyLinkedList sLL = new SinglyLinkedList();
+        sLL.createSinglyLinkedList(5);
+        //System.out.println(sLL.head.value);
+        sLL.insertInLinkedList(6, 1);
+        sLL.insertInLinkedList(7, 2);
+        sLL.insertInLinkedList(10, 4);
+        sLL.insertInLinkedList(8, 5);
+        sLL.insertInLinkedList(11  , 3);
+        //System.out.println(sLL.head.next.next.value);
+        sLL.traversalSinglyLinkedList();
+        sLL.searchNode(7);
+        sLL.searchNode(6);
+        sLL.searchNode(11);
+    }
+}
+
+..................................
+Node.java
+..................................
+public class Node {
+    public int value;
+    public Node next;
+    
+}
+
+.................................
+SinglyLinkedList.java
+.................................
+public class SinglyLinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
+
+    public Node createSinglyLinkedList(int nodeValue){
+        head = new Node();
+        Node node = new Node();
+        node.next = null;
+        node.value = nodeValue;
+        head = node;
+        tail = node;
+        size = 1;
+        return head;
+    }
+
+    // Insert Method Singly Linked List 
+    public void insertInLinkedList(int nodeValue, int location){
+        Node node = new Node();
+        node.value = nodeValue;
+        if(head==null){
+            createSinglyLinkedList(nodeValue);
+            return;
+        }else if(location == 0){
+            node.next = head;
+            head = node;
+        } else if(location >=size){
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        }else{
+            Node tempNode = head;
+            int index = 0;
+            while(index < location -1){
+                tempNode = tempNode.next;
+                index++;
+            }
+            Node nextNode = tempNode.next;
+            tempNode.next = node;
+            node.next = nextNode;
+        }
+        size++;
+    }
+    //Traversal in singly linked list
+
+    public void traversalSinglyLinkedList(){
+        if(head == null){
+            System.out.println("SLL does not exists");
+            
+        }else{
+            Node tempNode = head;
+            for(int i = 0;i<size;i++){
+                System.out.print(tempNode.value);
+                if(i != size-1){
+                    System.out.print("->");
+                }
+                tempNode = tempNode.next;
+            }
+        }
+        System.out.println("\n");
+    }
+
+    //Search for a node 
+
+    boolean searchNode(int nodeValue){
+        if(head != null){
+            Node tempNode = head;
+            for(int i =0; i<size;i++){
+                if(tempNode.value == nodeValue){
+                    System.out.print("Found the node at location " +i+"\n");
+                    return true;
+                }
+                tempNode = tempNode.next;
+            }
+        }
+        System.out.print("Node not found!");
+        return false;
+    }
+}
+
+
+...........
+//////finish///////////
